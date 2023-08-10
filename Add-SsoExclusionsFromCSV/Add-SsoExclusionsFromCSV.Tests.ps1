87e387d3bacfd1ebc753a03d1fa21edf113d43b8
christian.jaeckle <christian.jaeckle@teamviewer.com>
2023-08-10 12:15:39 +0200

    Changes parameter to "path", copyright, formatting

--- Add-SsoExclusionsFromCSV/Add-SsoExclusionsFromCSV.Tests.ps1
+++ Add-SsoExclusionsFromCSV/Add-SsoExclusionsFromCSV.Tests.ps1
@@ -1,18 +1,14 @@
-﻿# Copyright (c) 2019-2021 TeamViewer GmbH
-# See file LICENSE.txt
+﻿# Copyright (c) 2019-2023 TeamViewer Germany GmbH
+# See file LICENSE
 
 BeforeAll {
     $testApiToken = [securestring]@{}
-    . "$PSScriptRoot\Add-SsoExclusionsFromCSV.ps1" `
-        -ApiToken $testApiToken `
-        -csvPAth "testPath" `
-        -HeaderName "Email" `
-        -InformationAction 'SilentlyContinue'
+    . "$PSScriptRoot\Add-SsoExclusionsFromCSV.ps1" -ApiToken $testApiToken -Path 'testPath' -HeaderName 'Email' -InformationAction 'SilentlyContinue'
 
     Mock Invoke-TeamViewerPing { $true }
     Mock Get-TeamViewerSsoDomain { @(
-            [PSCustomObject]@{Id = '9602c5f4-2779-4f9a-80e8-4829531789fe'; Name = "example1.org" },
-            [PSCustomObject]@{Id = '33ad81bb-e88b-46d0-92e1-b4f1663abf31'; Name = "example2.org" }
+            [PSCustomObject]@{Id = '9602c5f4-2779-4f9a-80e8-4829531789fe'; Name = 'example1.org' },
+            [PSCustomObject]@{Id = '33ad81bb-e88b-46d0-92e1-b4f1663abf31'; Name = 'example2.org' }
         )
     }
     Mock Add-TeamViewerSsoExclusion {}
@@ -22,9 +18,9 @@ BeforeAll {
         "user1@example1.org","User1"
         "user2@example1.org","User2"
         "user3@example2.org","User3"
-        "user4@example2.org","User4" 
+        "user4@example2.org","User4"
         "user5@example2.org","User5"
-        "user5@example3.org","User6" 
+        "user5@example3.org","User6"
 '@
     }
 
@@ -54,16 +50,12 @@ BeforeAll {
 Describe 'Add-SsoExclusionsFromCSV' {
 
     It 'Should blah' {
-        Add-SsoExclusionsFromCSV -csvPath "example.csv" -HeaderName "Email"
+        Add-SsoExclusionsFromCSV -Path 'example.csv' -HeaderName 'Email'
 
         Assert-MockCalled Get-TeamViewerSsoDomain -Times 1 -Scope It
         Assert-MockCalled Add-TeamViewerSsoExclusion -Times 1 -Scope It -ParameterFilter {
-            $ApiToken -eq $testApiToken -And `
-                $DomainId -eq [guid]'9602c5f4-2779-4f9a-80e8-4829531789fe' -And `
-                $Email.Count -eq 2 }
+            $ApiToken -eq $testApiToken -And $DomainId -eq [guid]'9602c5f4-2779-4f9a-80e8-4829531789fe' -And $Email.Count -eq 2 }
         Assert-MockCalled Add-TeamViewerSsoExclusion -Times 1 -Scope It -ParameterFilter {
-            $ApiToken -eq $testApiToken -And `
-                $DomainId -eq [guid]'33ad81bb-e88b-46d0-92e1-b4f1663abf31' -And `
-                $Email.Count -eq 3 }
+            $ApiToken -eq $testApiToken -And $DomainId -eq [guid]'33ad81bb-e88b-46d0-92e1-b4f1663abf31' -And $Email.Count -eq 3 }
     }
 }
--- Add-SsoExclusionsFromCSV/README.md
+++ Add-SsoExclusionsFromCSV/README.md
@@ -1,6 +1,6 @@
 # Add-SsoExclusionsFromCSV
 
-Adds users from provided CSV file to SSO exclusion list of their respective domain
+Adds users from a CSV file to the TeamViewer SSO exclusion list of their respective domain.
 
 ## Prerequisites
 
@@ -15,20 +15,20 @@ Install-Module TeamViewerPS
 ### Import users from a CSV file
 
 ```powershell
-.\Add-SsoExclusionsFromCSV -csvPath 'c:\ps playground\test.csv' -HeaderName 'Email'
+.\Add-SsoExclusionsFromCSV -Path 'c:\Example.csv' -HeaderName 'Email'
 ```
 
-### Import users from a CSV file that uses semi-colon as delimiter. Use the given API token
+### Import users from a CSV file and use the given API token
 
 ```powershell
 $apiToken = 'SecretToken123' | ConvertTo-SecureString -AsPlainText -Force
-.\Add-SsoExclusionsFromCSV -csvPath 'c:\ps playground\test.csv' -HeaderName 'Email'
+.\Add-SsoExclusionsFromCSV -Path 'c:\Example.csv' -HeaderName 'Email'
 ```
 
-### Run the import script in "Test Mode" to see the changes that would be made.
+### Run the import script in "Test Mode" to see the changes that would be made
 
 ```powershell
-.\Add-SsoExclusionsFromCSV -csvPath 'c:\ps playground\test.csv' -HeaderName 'Email' -WhatIf
+.\Add-SsoExclusionsFromCSV -Path 'c:\Example.csv' -HeaderName 'Email' -WhatIf
 ```
 
 ## More help
